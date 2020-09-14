cef71710f7cf825512bb599a7d39ac2f05e2cff4
Deonte Archie <deontearchie@icloud.com>
2020-09-14 18:02:58 -0400

    Finish my burger website

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('wopper');
+INSERT INTO burgers (burger_name) VALUES ('quaterPounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('californiaburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('baconburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('salmonburger', true);
+INSERT INTO burgers (burger_name) VALUES ('turkeyburger');
